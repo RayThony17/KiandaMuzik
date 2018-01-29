750b72cc57e9ebbe63e2a33d47266579cda6f0a7
Ray thony <34481760+RayThony17@users.noreply.github.com>
2018-01-29 15:38:27 +0100

    Adicionei Retrofit e Retrofit-Converter ao projecto

--- app/src/main/java/api/Cliente.java
+++ app/src/main/java/api/Cliente.java
@@ -1,8 +1,20 @@
 package api;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 /**
  * Created by Ray Thony on 29/01/2018.
  */
 
+
 public class Cliente {
+    public interface PopularTracks {
+        @GET("popular")
+        Call<List<PopularTracks>> GetPopularTracks();
+
+
+    }
 }
